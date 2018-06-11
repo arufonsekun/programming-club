1d120bfab3e7dc122a4df530e680b2da30a01884
junior-16 <junior.ramisch@gmail.com>
2018-06-11 01:41:31 -0300

    Problems About Arrays

--- C++/Begginers/problems4.cpp
+++ C++/Begginers/problems4.cpp
@@ -227,6 +227,64 @@ void arrayFill3(){
 	}
 }
 
+vector<int> show(vector<int> array){
+	for (int i = 0; i < array.size(); i++){
+		if (array[1] % 2 == 0)printf("par[%d] = %d\n", i,array[i]);
+		else printf("impar[%d] = %d\n", i,array[i]);
+	}
+	array.clear();
+	return array;
+}
+
+void evenOddArray(){
+		vector<int> even;
+		vector<int> odd;
+		int x;
+		for (int i = 0; i < 15; i++){
+			cin >> x;
+			if (x % 2 == 0){
+				even.push_back(x);
+				if (even.size() == 5){
+					even = show(even);
+				}
+			}
+			else{
+				odd.push_back(x);
+				if (odd.size() == 5){
+					odd = show(odd);
+				}
+			}
+		}
+		if (odd.size() != 0) show(odd);
+		if (even.size() != 0) show(even);
+}
+
+void lowestValue(){
+	vector<int> array;
+	int x;
+	int value;
+	cin >> x;
+	for (int i = 0; i < x; i++){
+		cin >> value;
+		array.push_back(value);
+	}
+	int lowest = array[0];
+	int pos = 0;
+	for (int i = 1; i < array.size(); i++){
+		if (array[i] < lowest){
+			lowest = array[i];
+			pos = i;
+		}
+	}
+	printf("Menor valor: %d\n", lowest);
+	printf("Posicao: %d\n", pos);
+}
+
+void biDimentionalVector(){
+	int matrix [12][12];
+
+}
+
 int main(){
 	//growing_sequence();
 	//exceeding_z();
@@ -241,6 +299,9 @@ int main(){
 	//perfect_number();
 	//array_replacement();
 	//arrayFill2();
-	arrayFill3();
+	//arrayFill3();
+	//evenOddArray();
+	//lowestValue();
+	biDimentionalVector();
 	return 0;
 }
